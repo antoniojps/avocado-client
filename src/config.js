2850618fbc27280c12f77e99a5f75204bc1c17dd
António Santos <antoniojps@ua.pt>
2019-01-29 21:24:37 +0000

    fix ENDPOINT at config file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const HTTP_PROTOCOL = 'http://';
-export const ENDPOINT =  `${HTTP_PROTOCOL}api.avocado.pt` 
-export const CLIENT_ORIGIN = process.env.NODE_ENV === 'production' ? `${HTTP_PROTOCOL}avocado.pt` : `${HTTP_PROTOCOL}avocado.local:3000`
 export const API_URL_BASE = 'api.avocado.pt/api/v1'
+export const ENDPOINT = `${HTTP_PROTOCOL}${API_URL_BASE}`
+export const CLIENT_ORIGIN = process.env.NODE_ENV === 'production' ? `${HTTP_PROTOCOL}avocado.pt` : `${HTTP_PROTOCOL}avocado.local:3000`
